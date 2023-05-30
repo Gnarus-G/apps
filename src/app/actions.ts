01907186655dadaa87d2272b63ac0e2a283fc4d7
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-05-29 20:36:01 -0400

    feat: revalidation

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,9 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import { NewPicture, PicturesTable, db } from "./lib/drizzle";
 
 export async function newPictures(values: NewPicture[]) {
-  return await db.insert(PicturesTable).values(values).returning();
+  const r = await db.insert(PicturesTable).values(values).returning();
+  revalidatePath("/");
+  return r;
 }
